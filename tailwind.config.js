72255abd1fe27e9a8ce18f2a8c19421ee76a13f3
Ulyanov Dmitry <dusteex@yandex.ru>
2023-07-03 18:47:13 +0300

    Added variables for colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      black: '#27272A',
+      greyDark: '#696969',
+      lavenderPurple: '#7E3AF2',
+      lavenderRed: '#E02424'
+    },
   },
   plugins: [],
 }
