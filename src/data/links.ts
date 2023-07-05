ec1dd33db372781cf0abbdd9e74eb1868bc75a70
Ulyanov Dmitry <dusteex@yandex.ru>
2023-07-05 18:43:41 +0300

    Add data files

---
+++ src/data/links.ts
@@ -0,0 +1,16 @@
+import { NavbarLink } from "../components/ui/molecular/Navbar";
+
+export const navlinks: NavbarLink[] = [
+    {
+        text: "Main",
+        href: "#main"
+    },
+    {
+        text: "About",
+        href: "#about"
+    },
+    {
+        text: "Get in touch",
+        href: "#discuss"
+    },
+]
