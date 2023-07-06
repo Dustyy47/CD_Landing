b669180c1642d997837d14370b078d968deeacce
Ulyanov Dmitry <dusteex@yandex.ru>
2023-07-06 17:43:19 +0300

    Interests component

---
+++ src/components/ui/molecular/Interests.tsx
@@ -0,0 +1,31 @@
+export interface InterestInfo {
+  iconSrc: string
+  title: string
+  subtitle: string
+}
+
+interface InterestsProps {
+  interests: InterestInfo[]
+}
+
+export function Interests({ interests }: InterestsProps) {
+  return (
+    <ul>
+      {interests.map((interest) => (
+        <div className='flex mb-11' key={interest.title}>
+          <div className='border-[0.125rem] rounded-full p-[1.125rem] border-lavenderPurple mr-4'>
+            <img
+              className='w-[1.5rem] h-[1.5rem]'
+              src={interest.iconSrc}
+              alt={interest.title}
+            />
+          </div>
+          <div>
+            <p className='subtitle'>{interest.subtitle}</p>
+            <p className='bodyText'>{interest.title}</p>
+          </div>
+        </div>
+      ))}
+    </ul>
+  )
+}
