8a8d25bdb5dff0e3c88dcd52fc991bf7a9dcf3f3
Ulyanov Dmitry <dusteex@yandex.ru>
2023-07-06 17:10:21 +0300

    [CL-8] Main screen

---
+++ src/components/screens/MainScreen.tsx
@@ -0,0 +1,29 @@
+import { screensData } from '../../data/screens'
+import { Button } from '../ui/atomaric/Button'
+import { Article } from '../ui/molecular/Article'
+
+export function MainScreen() {
+  const { title, subtitle, article, buttonText } = screensData.main
+
+  return (
+    <section id='main' className='h-[48.125rem] relative overflow-hidden '>
+      <div className='container relative h-full z-50'>
+        <div className='w-[27.8125rem] pt-40'>
+          <h1 className='h1'>{title}</h1>
+          <p className='bodyText text-greyDark mb-[5.75rem]'>{subtitle}</p>
+          <div className='mb-[3.75rem]'>
+            <Article>{article}</Article>
+          </div>
+          <div className='w-48'>
+            <Button>{buttonText}</Button>
+          </div>
+        </div>
+      </div>
+      <img
+        className='z-0 block absolute bottom-[-6.9375rem] right-[-13.8rem]'
+        src={'./images/photo.png'}
+        alt=''
+      />
+    </section>
+  )
+}
---
+++ src/data/screens.tsx
@@ -0,0 +1,24 @@
+export const screensData = {
+  main: {
+    title: 'Hey, I’m Nick',
+    subtitle: 'Frontend developer',
+    article: (
+      <>
+        Help you to create high-quality digital products that your clients will
+        love and let your business thrive
+      </>
+    ),
+    buttonText: 'Get in touch'
+  },
+  aboutMe: {
+    title: 'About me',
+    article: (
+      <>
+        <b className='text-black'>Nick Richardson</b> - specialist in Frontend
+        development. Clear code is my passion. Solving issues through
+        negotiations
+      </>
+    ),
+    buttonText: 'Get in touch'
+  }
+}
