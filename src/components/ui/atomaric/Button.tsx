88ffa7553b41d0f99cf6bdc5df59531af3eeb679
Ulyanov Dmitry <dusteex@yandex.ru>
2023-07-06 17:11:18 +0300

    Updated button, removed articles

--- src/components/ui/atomaric/Button.tsx
+++ src/components/ui/atomaric/Button.tsx
@@ -2,9 +2,11 @@ import { ButtonHTMLAttributes } from 'react'
 import { SendIcon } from '../icons/SendIcon'
 import classNames from 'classnames'
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  href?: string
+}
 
-export function Button({ children, disabled, ...props }: ButtonProps) {
+export function Button({ children, disabled, href, ...props }: ButtonProps) {
   const baseStyles =
     'border-[3px] border-[transparent] flex items-center justify-between py-[0.8125rem] px-[1.5rem] h-[3.125rem] w-full rounded-[3.125rem]  duration-300'
   const stateButtonStyles = disabled
@@ -22,11 +24,13 @@ export function Button({ children, disabled, ...props }: ButtonProps) {
   )
 
   return (
-    <button {...props} className={classNames(baseStyles, stateButtonStyles)}>
-      <p className={textStyles}>{children as string}</p>
-      <SendIcon
-        classname={classNames(baseSendIconStyles, stateSendIconStyles)}
-      />
+    <button className='w-full' {...props}>
+      <a href={href} className={classNames(baseStyles, stateButtonStyles)}>
+        <p className={textStyles}>{children as string}</p>
+        <SendIcon
+          classname={classNames(baseSendIconStyles, stateSendIconStyles)}
+        />
+      </a>
     </button>
   )
 }
