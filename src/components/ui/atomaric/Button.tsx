2d6301741f83e5cca2c9ccf151ee5d2be9c14370
Ulyanov Dmitry <dusteex@yandex.ru>
2023-07-07 17:00:21 +0300

    Updated components for forms

--- src/components/ui/atomaric/Button.tsx
+++ src/components/ui/atomaric/Button.tsx
@@ -10,7 +10,7 @@ export function Button({ children, disabled, href, ...props }: ButtonProps) {
   const baseStyles =
     'border-[3px] border-[transparent] flex items-center justify-between py-[0.8125rem] px-[1.5rem] h-[3.125rem] w-full rounded-[3.125rem]  duration-300'
   const stateButtonStyles = disabled
-    ? 'bg-greyLight text-greyDark'
+    ? 'bg-greyLight text-greyDark cursor-not-allowed'
     : 'hover:bg-lavenderPurpleDark active:border-lavenderPurpleLight bg-lavenderPurple'
 
   const baseSendIconStyles = 'f w-[1.5rem]'
--- src/components/ui/atomaric/Textarea.tsx
+++ src/components/ui/atomaric/Textarea.tsx
@@ -1,34 +1,26 @@
 import classNames from 'classnames'
-import { TextareaHTMLAttributes } from 'react'
+import { TextareaHTMLAttributes, forwardRef } from 'react'
 
-interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
-  value: string
-  setValue: (v: string) => void
-}
+interface TextareaProps
+  extends Omit<TextareaHTMLAttributes<HTMLTextAreaElement>, 'ref'> {}
 
-export function Textarea({
-  value,
-  setValue,
-  disabled,
-  placeholder
-}: TextareaProps) {
-  const baseStyles =
-    'w-full duration-300 bodyText2 text-black py-[0.8125rem] px-5 placeholder:greyDark rounded-2xl h-[7.625rem] border-[0.08125rem] border-greyLight'
-  const stateStyles = classNames(
-    disabled && 'bg-greyLight border-none',
-    !disabled &&
-      'focus:border-lavenderPurple focus:outline-none hover:border-lavenderPurple'
-  )
+export const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ disabled, placeholder }: TextareaProps, ref) => {
+    const baseStyles =
+      'w-full resize-none duration-300 bodyText2 text-black py-[0.8125rem] px-5 placeholder:greyDark rounded-2xl h-[7.625rem] border-[0.08125rem] border-greyLight'
+    const stateStyles = classNames(
+      disabled && 'bg-greyLight border-none',
+      !disabled &&
+        'focus:border-lavenderPurple focus:outline-none hover:border-lavenderPurple'
+    )
 
-  return (
-    <textarea
-      disabled={disabled}
-      value={value}
-      onChange={(e) => setValue(e.target.value)}
-      placeholder={placeholder}
-      className={classNames(baseStyles, stateStyles)}
-    >
-      {value}
-    </textarea>
-  )
-}
+    return (
+      <textarea
+        disabled={disabled}
+        placeholder={placeholder}
+        className={classNames(baseStyles, stateStyles)}
+        ref={ref}
+      ></textarea>
+    )
+  }
+)
