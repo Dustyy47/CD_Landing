814e5c46e78f41f52982688b633ca9a5d07b7d42
Dmitry <109687670+Dustyy47@users.noreply.github.com>
2023-07-07 23:46:11 +0300

    pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { Header } from './components/ui/molecular/Header'
-import { MainScreen } from './components/screens/MainScreen'
 import { AboutScreen } from './components/screens/AboutScreen'
 import { DiscussScreen } from './components/screens/DiscussScreen'
+import { MainScreen } from './components/screens/MainScreen'
+import { Header } from './components/ui/molecular/Header'
 
 function App() {
   return (
     <>
       <Header />
-      <div>
-        <MainScreen />
-        <AboutScreen />
-        <DiscussScreen />
-      </div>
+      <MainScreen />
+      <AboutScreen />
+      <DiscussScreen />
     </>
   )
 }
