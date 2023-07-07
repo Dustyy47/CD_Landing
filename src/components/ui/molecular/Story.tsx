e5a6be7999cf8279e02bbc35027f541dfaae8700
Ulyanov Dmitry <dusteex@yandex.ru>
2023-07-07 17:04:23 +0300

    key for story component

--- src/components/ui/molecular/Story.tsx
+++ src/components/ui/molecular/Story.tsx
@@ -11,8 +11,11 @@ interface StoryProps {
 export function Story({ items }: StoryProps) {
   return (
     <ul>
-      {items.map((item) => (
-        <div className='flex last:border-none border-b-[1px] pt-6 pb-8 border-greyLight'>
+      {items.map((item, index) => (
+        <div
+          key={index}
+          className='flex last:border-none border-b-[1px] pt-6 pb-8 border-greyLight'
+        >
           <div className='w-[9.5rem] mr-4'>
             <p className='bodyText'>
               {item.start} - {item.end}
