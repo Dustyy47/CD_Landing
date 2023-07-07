392626cff4e3b87e2fe0bb519359caf959f4846d
Ulyanov Dmitry <dusteex@yandex.ru>
2023-07-07 18:52:12 +0300

    Main screen

--- src/components/ui/molecular/Article.tsx
+++ src/components/ui/molecular/Article.tsx
@@ -8,7 +8,9 @@ export function Article({ children }: ArticleProps) {
   return (
     <div>
       <span className='border-b-[0.1875rem] w-[1.5625rem] block mb-[1.5rem] border-lavenderPurple' />
-      <article className='text-greyDark bodyText'>{children}</article>
+      <article className='text-left text-greyDark lg:bodyText md:bodyText sm:mob_bodyText'>
+        {children}
+      </article>
     </div>
   )
 }
